ddb7afc413fcff24308d47aab3d8f681f84d0ad4
Jungea <jungea2241@gmail.com>
2022-01-23 21:15:20 +0900

    Spring Starter Project 생성
    pom.xml (jstl, tomcat)
    application.properties (prefix, suffix, port)
    Controller
    WEB-INF/views/index

---
+++ src/test/java/kr/co/mytutorial/TutorialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.mytutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
